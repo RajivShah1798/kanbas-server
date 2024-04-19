e0a1284b56bcf7a903b7b8631583f8314d7bf65a
Rajiv Shah <shah.rajiv1702@gmail.com>
2024-04-19 15:58:23 -0400

    Assignment6 Changes

--- App.js
+++ App.js
@@ -1,18 +1,21 @@
 import express from "express";
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
+import UserRoutes from "./Kanbas/Users/routes.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import cors from "cors";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import AssignmentRoutes from "./Kanbas/assignments/routes.js";
 
-
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas");
 const app = express();
 app.use(cors());
 app.use(express.json());
+UserRoutes(app);
 AssignmentRoutes(app);
 ModuleRoutes(app);
 CourseRoutes(app);
 Lab5(app);
 Hello(app);
-app.listen(process.env.PORT || 4000);
+app.listen(4000);
